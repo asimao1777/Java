e0114e8499789142e483aa0984741cb780ef0d30
asimao1777 <asimao@me.com>
2022-11-24 11:22:32 -0500

    "Exercise Try Catch for CS1"

---
+++ trycatch.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class trycatch {
+    public static void main(String[] args){
+        try {
+         Scanner input = new Scanner(System.in);
+         System.out.println("Please insert integer x: ");
+         int x = input.nextInt();
+         System.out.println(" Please insert integer y: ");
+         int y = input.nextInt();
+         System.out.format("x modulus y is equal to %d%n", x%y); 
+         input.close();
+
+        } catch (InputMismatchException a){
+            System.out.println("You need to input an integer");
+            System.exit(0);
+        }
+    }
+    
+}
